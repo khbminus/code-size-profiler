635f41a2b31e58ddfec6631c9a6f1dee21cebccb
Sergei Kharitontcev-Beglov <khbminus@gmail.com>
2023-08-29 14:45:37 +0200

    Add run & npm init task

--- gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/wasm/sizeprofiler/gradle/CodeSizeProfilerGradlePlugin.kt
+++ gradle-plugin/src/main/kotlin/org/jetbrains/kotlin/wasm/sizeprofiler/gradle/CodeSizeProfilerGradlePlugin.kt
@@ -2,13 +2,14 @@ package org.jetbrains.kotlin.wasm.sizeprofiler.gradle
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Exec
 import org.jetbrains.kotlin.gradle.targets.js.ir.KotlinJsIrLink
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 class CodeSizeProfilerGradlePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val downloadVisualization = project.tasks.register("downloadVisualization", VisualizationDownloaderTask::class.java)
-        project.tasks.register("processDump", DumpProcessingTask::class.java) {
+        val processDumpTask = project.tasks.register("processDump", DumpProcessingTask::class.java) {
             val linkTask = project
                 .tasks
                 .withType(KotlinJsIrLink::class.java)
@@ -16,6 +17,23 @@ class CodeSizeProfilerGradlePlugin : Plugin<Project> {
             it.inputDir.set(linkTask.destinationDirectory.get())
             it.dependsOn(linkTask)
             it.dependsOn(downloadVisualization)
+        }
+
+        val npmInstallTask = project.tasks.register("visualizationNPMInstall", Exec::class.java) {
+            it.workingDir = project.buildDir.resolve(VisualizationDownloaderTask.appLocation)
+            it.commandLine = listOf("npm", "install", "--quiet", "--no-audit", "--no-fund")
+            it.dependsOn(downloadVisualization)
+        }
+        val buildVisualizationTask = project.tasks.register("buildVisualization", Exec::class.java) {
+            it.workingDir = project.buildDir.resolve(VisualizationDownloaderTask.appLocation)
+            it.commandLine = listOf("npm", "run", "build")
+            it.dependsOn(npmInstallTask)
+            it.dependsOn(processDumpTask)
+        }
+        project.tasks.register("runVisualization", Exec::class.java) {
+            it.workingDir = project.buildDir.resolve(VisualizationDownloaderTask.appLocation)
+            it.commandLine = listOf("npm", "start")
+            it.dependsOn(buildVisualizationTask)
         }
 
         val addArgsTask = project.tasks.create("addCompilerArgs", AddCompilerArgsTask::class.java)
